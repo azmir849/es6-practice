6fe9e57bd3e6ff3529b5bdf109533f2b644bc922
azmir849 <azmir15-849@diu.edu.bd>
2020-08-11 12:52:10 +0600

    use of const and let

---
+++ letconst.js
@@ -0,0 +1,17 @@
+const numbers = [12,45];
+numbers[1]=88;
+numbers.push(50);
+console.log(numbers);
+
+let treeName = "kola gas";
+treeName = "jam gas";
+console.log(treeName);
+
+let sum = 0;
+for(let i= 0 ; i<=10 ; i++){
+    sum = sum + i;
+    console.log(i);
+    console.log(sum);
+
+}
+//console.log(i); not valid .Because let of i does not leacked outside
