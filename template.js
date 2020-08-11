0ca5cf33d4f65cc9aa1de106c0b63fb971a80be0
azmir849 <azmir15-849@diu.edu.bd>
2020-08-11 13:20:32 +0600

    use of template and multiline

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+
+const fullName=firstName+ " "+lastName;
+const fullName2 =`${firstName} ${lastName} ${10+40+50} is a good boy`;
+
+const multiline = `I love you
+I miss You
+I need You
+No. don't need you
+Akn R dorkar nai.`
+
+console.log(fullName);
+console.log(fullName2);
+console.log(multiline);
